7259dd78aad68179d62ee3470c74c68574908f92
Héuller Soares Vilela Silva <hllrdev@gmail.com>
2023-09-01 10:23:30 -0300

    Update UserController.java

--- src/main/java/com/service/zefu/controllers/UserController.java
+++ src/main/java/com/service/zefu/controllers/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.service.zefu.services.UserService;
 
 @RestController
-@CrossOrigin(origins = "https://client-zefu.onrender.com")
+@CrossOrigin(origins = "https://zefu.com.br")
 @RequestMapping("/users")
 public class UserController {
 
